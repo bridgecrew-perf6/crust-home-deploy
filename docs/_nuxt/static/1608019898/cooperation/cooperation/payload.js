1fa590a12f71f9a031788cc6712f77230fb73e1a
gavfu <gavfu@outlook.com>
2020-12-15 08:11:50 +0000

    Automated release: 2020-12-15, 08:11:50

---
+++ docs/_nuxt/static/1608019898/cooperation/cooperation/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/cooperation/cooperation", {data:[{}],fetch:[],mutations:[]});
---
+++ docs/_nuxt/static/1608019898/cooperation/cooperation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1608019898",layout:"default",error:a,state:{locales:["zh",b],locale:b,type:"",id:a},serverRendered:true,routePath:"\u002Fcooperation\u002Fcooperation",config:{}}}(null,"en"));

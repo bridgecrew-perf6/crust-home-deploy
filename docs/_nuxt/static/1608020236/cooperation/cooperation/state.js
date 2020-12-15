0f13b3c3ee67296e28aa591e962289590310e116
gavfu <gavfu@outlook.com>
2020-12-15 08:17:27 +0000

    Automated release: 2020-12-15, 08:17:27

---
+++ docs/_nuxt/static/1608020236/cooperation/cooperation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1608020236",layout:"default",error:a,state:{locales:["zh",b],locale:b,type:"",id:a},serverRendered:true,routePath:"\u002Fcooperation\u002Fcooperation",config:{}}}(null,"en"));

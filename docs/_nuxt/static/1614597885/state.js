e329625dc8ff8976ae65215f4b935c64a3a8fd71
gavfu <gavfu@outlook.com>
2021-03-01 11:25:03 +0000

    Automated release: 2021-03-01, 11:25:03

---
+++ docs/_nuxt/static/1614597885/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614597885",layout:"default",error:a,state:{locales:["zh",b],locale:b,type:"",id:a},serverRendered:true,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en","\u002F"));

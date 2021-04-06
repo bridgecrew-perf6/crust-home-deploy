9c4c8283c0877ee6af883d43d169b94c9899fd2d
fanxy1995 <75827885+fanxy1995@users.noreply.github.com>
2021-04-06 09:24:31 +0000

    Automated release: 2021-04-06, 09:24:31

---
+++ docs/_nuxt/static/1617701046/cooperation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617701046",layout:"default",error:a,state:{locales:["zh",b],locale:b,type:"",id:a},serverRendered:true,routePath:"\u002Fcooperation",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
